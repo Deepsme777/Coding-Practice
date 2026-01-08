8e5963bdf97781e9922e19a04720caa85ab4f668
Deepsme777 <deeps.me2006@gmail.com>
2026-01-08 14:11:57 +0530

    Add binary, hexadecimal, and octal conversion

---
+++ Binary values.py
@@ -0,0 +1,7 @@
+# value to an binary value
+# by string format method 
+value = 1034
+
+print("{:b}".format(value))   # binary
+print("{:X}".format(value))   # hexadecimal
+print("{:o}".format(value))   # Octadecimal
