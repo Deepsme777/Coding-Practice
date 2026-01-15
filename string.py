3b5f7a6c34f23ffe6555027ce0d2f787628b24a0
Deepsme777 <deepakkumarabi121@gmail.com>
2026-01-15 12:50:49 +0530

    String

---
+++ string.py
@@ -0,0 +1,22 @@
+Name = "G Deepakkumar"
+print(Name)
+print(type(Name))
+age = 19
+print(age)
+print(type(age))
+
+# add str + int
+age = str(age)
+print(Name + " " + age)
+print("Your Name is : " + Name)
+print("Your age is : " + age)
+print("No of alpha in your name : " + str(len(Name)))
+
+name_1 = "lakshaya"
+print(Name.find("a"))
+print(name_1.capitalize())
+print(name_1.upper())
+print(name_1.count("a"))
+print(name_1.isdigit())
+print(name_1.isalpha())
+print(name_1.replace("a" , "A"))
