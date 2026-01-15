8c4c54eb52d23f8cd85d9961066382e3b43b991e
Deepsme777 <deepakkumarabi121@gmail.com>
2026-01-15 13:15:21 +0530

    added maxd min

--- Mathfunction.py
+++ Mathfunction.py
@@ -1,1 +1,23 @@
-print("kell")
+import math
+pi = 3.14
+A = -5.14
+
+print(round(pi))
+print(round(9.36535))
+print(abs(A))
+print("_________________________")
+print(math.ceil(pi))
+print(math.floor(pi))
+print("_____________________________")
+print(pow(pi,2))
+print(math.sqrt(25))
+print("----------------------")
+print(math.factorial(5))
+
+print("----------------------")
+
+x = 1
+y = 3
+
+print(max(x,y))
+print(min(x,y))
