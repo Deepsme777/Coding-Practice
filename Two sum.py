a23a14c6b2c97d0093cd37db7d79cab0c12d9c67
Deepsme777 <deeps.me2006@gmail.com>
2026-04-03 14:56:46 +0530

    added two sums list

---
+++ Two sum.py
@@ -0,0 +1,29 @@
+# for some list it will return the list ordered value
+
+class solutions :
+
+        u = input("Select an Value(0,1,2)  : ")
+        
+        if u == "0" :
+                print("you have selected 0")
+
+                list = [2,7,11,15]
+                n = list[0] + list[1]
+                print("the sum of the list is : ",n)
+
+        elif u == "1" :
+                print("you have selected 1")
+
+                list = [3,2,4]
+                n = list[0] + list[1]
+                print("the sum of the list is : ",n)
+
+        elif u == "2" :
+                print("you have selected 2")
+
+                list = [3,3]
+                n = list[0] + list[1]
+                print("the sum of the list is : ",n)
+        else :
+            print("Choose in the following")
+
