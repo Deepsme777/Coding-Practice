9a2c6bce650dd15df02924f1ee3a85060422e793
Deepsme777 <deepakkumarabi121@gmail.com>
2026-05-17 14:02:25 +0530

    done some changes

--- Binary values.py
+++
@@ -1,7 +0,0 @@
-# value to an binary value
-# by string format method 
-value = 1034
-
-print("{:b}".format(value))   # binary
-print("{:X}".format(value))   # hexadecimal
-print("{:o}".format(value))   # Octadecimal
